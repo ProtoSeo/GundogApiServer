df3b60c1313c941945080ce2026ec00fa2458eeb
SeoSeunghun <proto_seo@naver.com>
2022-05-23 23:27:00 +0900

    [init] project

---
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -0,0 +1,16 @@
+package com.example.plugins
+
+import io.ktor.serialization.gson.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+    routing {
+    }
+}
