d459cf56080eedd04c7a686d4cd3e3d87c45a109
SeoSeunghun <proto_seo@naver.com>
2022-05-31 23:58:13 +0900

    [feat][#1] Member Dto 구현

---
+++ src/main/kotlin/com/example/member/dto/MemberLoginResponse.kt
@@ -0,0 +1,4 @@
+package com.example.member.dto
+
+data class MemberLoginResponse(val email: String, val token: String)
+
---
+++ src/main/kotlin/com/example/member/dto/MemberRequest.kt
@@ -0,0 +1,3 @@
+package com.example.member.dto
+
+data class MemberRequest(val email: String, val password: String)
