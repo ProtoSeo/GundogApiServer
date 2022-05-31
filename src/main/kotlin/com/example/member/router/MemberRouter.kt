c5e8c6bd6c70b3462c9de398364d30fad5754756
SeoSeunghun <proto_seo@naver.com>
2022-05-31 23:59:25 +0900

    [feat][#1] Member Router 구현

---
+++ src/main/kotlin/com/example/member/router/MemberRouter.kt
@@ -0,0 +1,42 @@
+package com.example.member
+
+import com.example.member.dto.MemberRequest
+import com.example.member.service.MemberService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Routing.memberRoute(memberService: MemberService) {
+    route("api/v1") {
+        post("login") {
+            val memberRequest = call.receive<MemberRequest>()
+            val login = memberService.login(memberRequest)
+            call.respond(login)
+        }
+        post("signup") {
+            val memberRequest = call.receive<MemberRequest>()
+            val id = memberService.register(memberRequest)
+            call.respond(status = HttpStatusCode.Created, id)
+        }
+        get("members") {
+            val email = call.request.queryParameters["email"].toString()
+            if (memberService.isDuplicateEmail(email)) {
+                call.respond(HttpStatusCode.Conflict)
+            } else {
+                call.respond(HttpStatusCode.NoContent)
+            }
+        }
+        authenticate {
+            get("members/auth") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("email").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
+            }
+        }
+    }
+}
