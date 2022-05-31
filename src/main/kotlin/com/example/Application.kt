182ad1104a033ffc86dfd92f5746242569638a37
SeoSeunghun <proto_seo@naver.com>
2022-05-31 23:58:54 +0900

    [feat][#1] 설정 관련 추가

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,16 +1,17 @@
 package com.example
 
+import com.example.common.config.*
 import io.ktor.server.application.*
-import com.example.plugins.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    configureDB()
     configureRouting()
     configureSecurity()
     configureHTTP()
-    configureMonitoring()
     configureSerialization()
+    configureException()
 }
