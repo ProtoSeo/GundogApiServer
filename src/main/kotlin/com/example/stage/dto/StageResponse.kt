25d56cff003416712b3de5fb8c551844718ebb49
SeoSeunghun <proto_seo@naver.com>
2022-06-01 15:12:14 +0900

    [feat][#3] Stage 관련 DTO 생성

---
+++ src/main/kotlin/com/example/stage/dto/StageResponse.kt
@@ -0,0 +1,3 @@
+package com.example.stage.dto
+
+data class StageResponse(val id: Long, val name: String, val level: Int)
---
+++ src/main/kotlin/com/example/stage/dto/StageSaveRequest.kt
@@ -0,0 +1,3 @@
+package com.example.stage.dto
+
+data class StageSaveRequest(val name: String, val level: Int)
