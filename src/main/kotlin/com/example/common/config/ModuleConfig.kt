5718aa0bbdf62db91d69f750c8eefbdffc8442fe
SeoSeunghun <proto_seo@naver.com>
2022-06-01 15:09:28 +0900

    [feat][#3] Stage, MemberStage 관련 DI, Router 설정 추가

--- src/main/kotlin/com/example/common/config/ModuleConfig.kt
+++ src/main/kotlin/com/example/common/config/ModuleConfig.kt
@@ -2,6 +2,10 @@ package com.example.common.config
 
 import com.example.member.repository.MemberRepository
 import com.example.member.service.MemberService
+import com.example.memberstage.repository.MemberStageRepository
+import com.example.memberstage.service.MemberStageService
+import com.example.stage.repository.StageRepository
+import com.example.stage.service.StageService
 import org.kodein.di.DI
 import org.kodein.di.bindSingleton
 import org.kodein.di.instance
@@ -12,7 +16,19 @@ object ModuleConfig {
         bindSingleton { MemberRepository() }
     }
 
+    private val stageModule = DI.Module("Stage") {
+        bindSingleton { StageService(instance()) }
+        bindSingleton { StageRepository() }
+    }
+
+    private val memberStageModule = DI.Module("MemberStage") {
+        bindSingleton { MemberStageService(instance()) }
+        bindSingleton { MemberStageRepository() }
+    }
+
     internal val kodein = DI {
         import(memberModule)
+        import(stageModule)
+        import(memberStageModule)
     }
 }
--- src/main/kotlin/com/example/common/config/RouterConfig.kt
+++ src/main/kotlin/com/example/common/config/RouterConfig.kt
@@ -1,15 +1,23 @@
 package com.example.common.config
 
-import com.example.member.memberRoute
+import com.example.member.router.memberRoute
 import com.example.member.service.MemberService
+import com.example.memberstage.router.memberStageRoute
+import com.example.memberstage.service.MemberStageService
+import com.example.stage.router.stageRouter
+import com.example.stage.service.StageService
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.kodein.di.instance
 
 fun Application.configureRouting() {
     val memberService: MemberService by ModuleConfig.kodein.instance()
+    val stageService: StageService by ModuleConfig.kodein.instance()
+    val memberStageService: MemberStageService by ModuleConfig.kodein.instance()
 
     routing {
         memberRoute(memberService)
+        memberStageRoute(memberStageService)
+        stageRouter(stageService)
     }
 }
