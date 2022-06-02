38b82dff30567a175d582a17bd2f9911608d0509
SeoSeunghun <proto_seo@naver.com>
2022-06-02 19:29:48 +0900

    [feat][#5] Item domain 생성

---
+++ src/main/kotlin/com/example/item/domain/Items.kt
@@ -0,0 +1,9 @@
+package com.example.item.domain
+
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object Items : LongIdTable(name = "item") {
+    val name = varchar(name = "name", length = 255)
+    val description = text("description")
+    val limit = long(name = "limit").default(Long.MAX_VALUE)
+}
