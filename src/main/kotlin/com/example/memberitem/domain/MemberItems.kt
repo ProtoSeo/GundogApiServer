313495ef809a22148e800b2386ac8c01a874e63e
SeoSeunghun <proto_seo@naver.com>
2022-06-02 19:30:08 +0900

    [feat][#5] MemberItem domain 생성

---
+++ src/main/kotlin/com/example/memberitem/domain/MemberItems.kt
@@ -0,0 +1,11 @@
+package com.example.memberitem.domain
+
+import com.example.item.domain.Items
+import com.example.member.domain.Members
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object MemberItems : LongIdTable(name = "member_item") {
+    val memberId = reference("member_id", Members)
+    val itemId = reference("item_id", Items)
+    val count = long("count").default(0)
+}
