cf1166dc79a8c2d2ddccc7ff73bdb4d14670ca29
SeoSeunghun <proto_seo@naver.com>
2022-06-03 23:40:27 +0900

    [feat][#6] CharacterItem domain 생성

---
+++ src/main/kotlin/com/example/membercharacter/domain/MemberCharacters.kt
@@ -0,0 +1,13 @@
+package com.example.membercharacter.domain
+
+import com.example.character.domain.Characters
+import com.example.member.domain.Members
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object MemberCharacters : LongIdTable("member_character") {
+    val memberId = reference("member_id", Members)
+    val characterId = reference("character_id", Characters)
+    val healthLevel = integer("health_level").default(0)
+    val staminaLevel = integer("stamina_level").default(0)
+    val isOpen = bool("is_open").default(false)
+}
