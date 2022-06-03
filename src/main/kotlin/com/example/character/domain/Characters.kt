2c76371aa39ba28f9c75d60fe9b291b995b131ea
SeoSeunghun <proto_seo@naver.com>
2022-06-03 23:39:15 +0900

    [feat][#6] Characters domain 생성

---
+++ src/main/kotlin/com/example/character/domain/Characters.kt
@@ -0,0 +1,9 @@
+package com.example.character.domain
+
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object Characters : LongIdTable("characters") {
+    val name = varchar("name", 100).uniqueIndex()
+    val health = integer("health").default(3)
+    val stamina = integer("stamina").default(10)
+}
