8abe498eb49c150c0fc0e8968489f203a38d98fc
SeoSeunghun <proto_seo@naver.com>
2022-06-05 15:06:59 +0900

    [refactor][#10] null 관련 예외처리

--- src/main/kotlin/com/example/membercharacter/service/MemberCharacterService.kt
+++ src/main/kotlin/com/example/membercharacter/service/MemberCharacterService.kt
@@ -1,22 +1,24 @@
 package com.example.membercharacter.service
 
 import com.example.membercharacter.dto.MemberCharacterLevelUpRequest
-import com.example.membercharacter.dto.MemberCharacterResponse
+import com.example.membercharacter.dto.MemberCharacter
+import com.example.membercharacter.exception.MemberCharacterException
+import com.example.membercharacter.exception.MemberCharacterExceptionType.*
 import com.example.membercharacter.repository.MemberCharacterRepository
 import io.ktor.server.auth.jwt.*
 
 class MemberCharacterService(private val memberCharacterRepository: MemberCharacterRepository) {
 
-    fun getMemberCharacters(principal: JWTPrincipal): List<MemberCharacterResponse> {
+    fun getMemberCharacters(principal: JWTPrincipal): List<MemberCharacter> {
         val memberId = principal.payload.getClaim("id").asLong()
         return memberCharacterRepository.findAllByMemberId(memberId)
     }
 
-    fun levelUpMemberCharacters(memberCharacterId: Long, request: MemberCharacterLevelUpRequest): MemberCharacterResponse {
-        return memberCharacterRepository.update(memberCharacterId, request)
+    fun levelUpMemberCharacters(memberCharacterId: Long, request: MemberCharacterLevelUpRequest): MemberCharacter {
+        return memberCharacterRepository.update(memberCharacterId, request) ?: throw MemberCharacterException(NOT_FOUND)
     }
 
-    fun openMemberCharacters(memberCharacterId: Long): MemberCharacterResponse {
-        return memberCharacterRepository.update(memberCharacterId)
+    fun openMemberCharacters(memberCharacterId: Long): MemberCharacter {
+        return memberCharacterRepository.update(memberCharacterId) ?: throw MemberCharacterException(NOT_FOUND)
     }
 }
