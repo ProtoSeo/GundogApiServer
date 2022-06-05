6a8a11a3234bd6109e172e199a8cf061f08fc31c
SeoSeunghun <proto_seo@naver.com>
2022-06-05 15:12:32 +0900

    [refactor][#10] null 관련 예외처리

--- src/main/kotlin/com/example/memberstage/service/MemberStageService.kt
+++ src/main/kotlin/com/example/memberstage/service/MemberStageService.kt
@@ -1,8 +1,10 @@
 package com.example.memberstage.service
 
-import com.example.memberstage.dto.MemberStageRankResponse
+import com.example.memberstage.dto.StageRankInfo
 import com.example.memberstage.dto.MemberStageRequest
-import com.example.memberstage.dto.MemberStageResponse
+import com.example.memberstage.dto.MemberStageInfo
+import com.example.memberstage.exception.MemberStageException
+import com.example.memberstage.exception.MemberStageExceptionType.*
 import com.example.memberstage.repository.MemberStageRepository
 import io.ktor.server.auth.jwt.*
 
@@ -18,11 +20,12 @@ class MemberStageService(private val memberStageRepository: MemberStageRepositor
         return if (isUpdate) "성공적으로 업데이트되었습니다." else "기존 점수가 높으므로 업데이트 되지 않습니다."
     }
 
-    fun getRankingToStage(stageId: Long): List<MemberStageRankResponse> {
+    fun getRankingToStage(stageId: Long): List<StageRankInfo> {
         return memberStageRepository.findRankingByStageId(stageId)
     }
 
-    fun getMemberStage(stageId: Long, memberId: Long): MemberStageResponse {
+    fun getMemberStage(stageId: Long, memberId: Long): MemberStageInfo {
         return memberStageRepository.findByMemberIdAndStageId(memberId, stageId)
+            ?: throw MemberStageException(MEMBER_STAGE_NOT_FOUND)
     }
 }
