51b773a7ffe78382bc801acbe76835ec14e3510b
SeoSeunghun <proto_seo@naver.com>
2022-06-05 14:20:58 +0900

    [refactor][#10] MemberException 관련 수정

--- src/main/kotlin/com/example/member/exception/MemberException.kt
+++ src/main/kotlin/com/example/member/exception/MemberException.kt
@@ -6,7 +6,7 @@ import com.example.common.exception.ExceptionType
 class MemberException(private val exceptionType: MemberExceptionType): BaseException() {
 
     override fun getExceptionType(): ExceptionType {
-        return exceptionType
+        return this.exceptionType
     }
 
 }
--- src/main/kotlin/com/example/member/exception/MemberExceptionType.kt
+++ src/main/kotlin/com/example/member/exception/MemberExceptionType.kt
@@ -3,13 +3,13 @@ package com.example.member.exception
 import com.example.common.exception.ExceptionType
 import io.ktor.http.*
 
-enum class MemberExceptionType(private val HttpStatusCode: HttpStatusCode, private val errorMsg: String) : ExceptionType {
+enum class MemberExceptionType(private val httpStatusCode: HttpStatusCode, private val errorMsg: String) : ExceptionType {
     DUPLICATE_EMAIL(HttpStatusCode.Conflict, "이메일이 중복되었습니다."),
     NOT_FOUND(HttpStatusCode.NotFound, "해당 회원이 존재하지 않습니다."),
     UN_AUTHORIZATION(HttpStatusCode.Unauthorized, "이메일 혹은 비밀번호가 잘못 입력되었습니다.");
 
     override fun getHttpStatus(): HttpStatusCode {
-        return this.HttpStatusCode
+        return this.httpStatusCode
     }
 
     override fun getErrorMessage(): String {
