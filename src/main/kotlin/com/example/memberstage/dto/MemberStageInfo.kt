99e4ee5060b956a974dee9d38b70775b63bb0892
SeoSeunghun <proto_seo@naver.com>
2022-06-05 15:11:19 +0900

    [refactor][#9] Dto 이름 수정

--- src/main/kotlin/com/example/memberstage/dto/MemberStageInfo.kt
+++ src/main/kotlin/com/example/memberstage/dto/MemberStageInfo.kt
@@ -1,3 +1,3 @@
 package com.example.memberstage.dto
 
-data class MemberStageResponse(val id: Long, val email: String, val bestScore: Long, val isClear: Boolean)
+data class MemberStageInfo(val id: Long, val email: String, val bestScore: Long, val isClear: Boolean)
