63b47f0d3bf8d2549e0a0bce6335355cf699b580
SeoSeunghun <proto_seo@naver.com>
2022-06-07 20:02:37 +0900

    [feat][#10] JWT token 관련 예외 생성

--- src/main/kotlin/com/example/member/exception/MemberExceptionType.kt
+++ src/main/kotlin/com/example/member/exception/MemberExceptionType.kt
@@ -6,7 +6,8 @@ import io.ktor.http.*
 enum class MemberExceptionType(private val httpStatusCode: HttpStatusCode, private val errorMsg: String) : ExceptionType {
     DUPLICATE_EMAIL(HttpStatusCode.Conflict, "이메일이 중복되었습니다."),
     NOT_FOUND(HttpStatusCode.NotFound, "해당 회원이 존재하지 않습니다."),
-    UN_AUTHORIZATION(HttpStatusCode.Unauthorized, "이메일 혹은 비밀번호가 잘못 입력되었습니다.");
+    BAD_REQUEST(HttpStatusCode.BadRequest, "이메일 혹은 비밀번호가 잘못 입력되었습니다."),
+    UN_AUTHORIZED(HttpStatusCode.Unauthorized, "토큰이 유효하지 않습니다.");
 
     override fun getHttpStatus(): HttpStatusCode {
         return this.httpStatusCode
