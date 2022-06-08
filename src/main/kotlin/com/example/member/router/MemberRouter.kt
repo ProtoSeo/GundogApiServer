d4909d1e880eab742db4cc5888230cea849a115c
SeoSeunghun <proto_seo@naver.com>
2022-06-08 23:58:11 +0900

    [feat][#10] jwt cookie에 담기, email 예외처리 추가

--- src/main/kotlin/com/example/member/router/MemberRouter.kt
+++ src/main/kotlin/com/example/member/router/MemberRouter.kt
@@ -1,8 +1,8 @@
 package com.example.member.router
 
+import com.example.member.domain.Members.email
 import com.example.member.dto.MemberRequest
 import com.example.member.exception.MemberException
-import com.example.member.exception.MemberExceptionType
 import com.example.member.exception.MemberExceptionType.*
 import com.example.member.service.MemberService
 import io.ktor.http.*
@@ -14,19 +14,30 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Routing.memberRoute(memberService: MemberService) {
+    val regex = Regex("^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$")
     route("api/v1") {
         post("login") {
             val memberRequest = call.receive<MemberRequest>()
-            val login = memberService.login(memberRequest)
-            call.respond(login)
+            val loginResponse = memberService.login(memberRequest)
+            call.response.cookies.append(
+                Cookie(
+                    "token",
+                    loginResponse.token,
+                    httpOnly = true,
+                    maxAge = 36_000_00 * 10
+                )
+            )
+            call.respond(loginResponse)
         }
         post("signup") {
             val memberRequest = call.receive<MemberRequest>()
+            if (!regex.containsMatchIn(memberRequest.email)) throw MemberException(INVALID_EMAIL)
             val id = memberService.register(memberRequest)
             call.respond(status = HttpStatusCode.Created, id)
         }
         get("members") {
             val email = call.request.queryParameters["email"].toString()
+            if (!regex.containsMatchIn(email)) throw MemberException(INVALID_EMAIL)
             if (memberService.isDuplicateEmail(email)) {
                 call.respond(HttpStatusCode.Conflict)
             } else {
