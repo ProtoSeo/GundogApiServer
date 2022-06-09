a16e9a2ab24914f7ca28d847e25843bf22239dd8
SeoSeunghun <proto_seo@naver.com>
2022-06-10 00:48:43 +0900

    [feat][#9] member Stage 관련 dto 변경

--- src/main/kotlin/com/example/memberstage/dto/MemberStageInfo.kt
+++
@@ -1,3 +0,0 @@
-package com.example.memberstage.dto
-
-data class MemberStageInfo(val id: Long, val email: String, val bestScore: Long, val isClear: Boolean)
---
+++ src/main/kotlin/com/example/memberstage/dto/StageInfo.kt
@@ -0,0 +1,3 @@
+package com.example.memberstage.dto
+
+data class StageInfo(val id: Long, val bestScore: Long, val isClear: Boolean, val isOpen: Boolean)
