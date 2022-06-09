33863a117b225fd8e708a633ce038f361c93485c
SeoSeunghun <proto_seo@naver.com>
2022-06-10 00:48:43 +0900

    [feat][#9] member Stage 관련 dto 변경

--- src/main/kotlin/com/example/memberstage/dto/StageRankInfo.kt
+++ src/main/kotlin/com/example/memberstage/dto/StageRankInfo.kt
@@ -1,3 +1,3 @@
 package com.example.memberstage.dto
 
-data class StageRankInfo(val id: Long, val email: String, val bestScore: Long)
+data class StageRankInfo(val ranking: Int, val email: String, val bestScore: Long)
