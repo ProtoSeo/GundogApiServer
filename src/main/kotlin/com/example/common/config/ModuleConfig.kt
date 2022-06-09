a038b00621904910c47376030ae2f8e9300c9592
SeoSeunghun <proto_seo@naver.com>
2022-06-09 18:30:01 +0900

    [fix][#9] Package 구조 변경 후 오류 해결

--- src/main/kotlin/com/example/common/config/ModuleConfig.kt
+++ src/main/kotlin/com/example/common/config/ModuleConfig.kt
@@ -8,8 +8,6 @@ import com.example.memberitem.repository.MemberItemRepository
 import com.example.memberitem.service.MemberItemService
 import com.example.memberstage.repository.MemberStageRepository
 import com.example.memberstage.service.MemberStageService
-import com.example.stage.repository.StageRepository
-import com.example.stage.service.StageService
 import org.kodein.di.DI
 import org.kodein.di.bindSingleton
 import org.kodein.di.instance
@@ -18,11 +16,6 @@ object ModuleConfig {
     private val memberModule = DI.Module("Member") {
         bindSingleton { MemberService(instance(), instance(), instance()) }
         bindSingleton { MemberRepository() }
-    }
-
-    private val stageModule = DI.Module("Stage") {
-        bindSingleton { StageService(instance()) }
-        bindSingleton { StageRepository() }
     }
 
     private val memberStageModule = DI.Module("MemberStage") {
@@ -42,7 +35,6 @@ object ModuleConfig {
 
     internal val kodein = DI {
         import(memberModule)
-        import(stageModule)
         import(memberStageModule)
         import(memberItemModule)
         import(memberCharacterModule)
