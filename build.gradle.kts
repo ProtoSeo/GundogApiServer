3d3117f2f0807278cdd77a36ba4536f74d790539
SeoSeunghun <proto_seo@naver.com>
2022-06-12 16:21:27 +0900

    [chore][#20] jar로 빌드하기 위한 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ val jbcryptVersion: String by project
 plugins {
     application
     kotlin("jvm") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.example"
